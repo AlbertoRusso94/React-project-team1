2d86c2fbdeb4ab8825e38022511ed4ae0352b5d9
f-marinelli <fabri.marinelli@gmail.com>
2022-06-03 10:44:37 +0200

    filesystem

--- react-team-1/src/App.js
+++ react-team-1/src/App.js
@@ -1,8 +1,8 @@
-import Footer from './Footer';
-import Navbar from './Navbar';
+import Footer from './components/Footer';
+import Navbar from './components/Navbar';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Homepage from './Homepage';
-import Dashboard from './Dashboard';
+import Homepage from './pages/Homepage';
+import Dashboard from './pages/Dashboard';
 
 function App() {
   return (
